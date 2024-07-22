108e8e2c1478633b1a37ea5c57d62083ccf26a55
Shien Lee <shienlee73@gmail.com>
2024-07-22 23:46:49 +0800

    fix migrate syntax error

--- db/migration/000002_add_users.down.sql
+++ db/migration/000002_add_users.down.sql
@@ -1,3 +1,3 @@
-ALTER TABLE IF EXISTS "accounts" DROP CONSTRAINT IF EXISTS "accounts_owner_fkey"
+ALTER TABLE IF EXISTS "accounts" DROP CONSTRAINT IF EXISTS "accounts_owner_fkey";
 
 DROP TABLE IF EXISTS users;
