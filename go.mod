93c89964f7fd6ccb0abad88e63afdc542b4ada0e
Samuel <Samuel@yuan.com.tw>
2024-08-07 17:50:32 +0800

    embed static files inside server's binary

--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.21.0
 	github.com/lib/pq v1.10.9
 	github.com/o1egl/paseto v1.0.0
+	github.com/rakyll/statik v0.1.7
 	github.com/spf13/viper v1.19.0
 	github.com/stretchr/testify v1.9.0
 	golang.org/x/crypto v0.25.0
--- main.go
+++ main.go
@@ -7,8 +7,10 @@ import (
 	"net"
 	"net/http"
 
+	_ "github.com/shienlee73/simplebank/doc/statik"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	_ "github.com/lib/pq"
+	"github.com/rakyll/statik/fs"
 	"github.com/shienlee73/simplebank/api"
 	db "github.com/shienlee73/simplebank/db/sqlc"
 	"github.com/shienlee73/simplebank/gapi"
@@ -63,8 +65,13 @@ func runGatewayServer(config util.Config, store db.Store) {
 	mux := http.NewServeMux()
 	mux.Handle("/", grpcMux)
 
-	fs := http.FileServer(http.Dir("./doc/swagger"))
-	mux.Handle("/swagger/", http.StripPrefix("/swagger/", fs))
+	statikFS, err := fs.New()
+	if err != nil {
+		log.Fatal("cannot create statik fs: %s", err)
+	}
+
+	swaggerHandler := http.StripPrefix("/swagger/", http.FileServer(statikFS))
+	mux.Handle("/swagger/", swaggerHandler)
 
 	listener, err := net.Listen("tcp", config.HTTPServerAddress)
 	if err != nil {
